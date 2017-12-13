43d8ec8c75c2f690ab713d5fb73d9de8712d4680
Jenny Gao <jgao658@gmail.com>
2017-12-13 14:49:30 -0500

    just need flash ending

--- find.js
+++ find.js
@@ -4,12 +4,13 @@ var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
-
+var targetX = boxWidth/2;//Math.random() * boxWidth;
+var targetY = boxHeight/2;//Math.random() * boxHeight;
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
+console.log( "target x: " + targetX );
+console.log( "target y: " + targetY );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
@@ -17,20 +18,50 @@ var distance = function (x0, y0, x1, y1) {
     return Math.sqrt(((x0 - x1) ** 2) + ((y0 - y1) ** 2));
 };
 
-
 var findIt = function(e) {
     /* YOUR IMPLEMENTATION */
-    var red = 0;
-    var blue = 0;
-    var green = 0;
-    var box = document.getElementById("box");
-    
+    var maxDist = maxDistance();
+    var mouseX = e.pageX;
+    var mouseY = e.pageY;
+    var l = 100 - Math.floor((distance(mouseX, mouseY, targetX, targetY) / maxDist) * 100);
+    if(l > 90){
+	l = 90;
+    }
+    box.style.backgroundColor = "hsl(182,62%,"+l+"%)";
+    if(isCloseEnough(mouseX,mouseY)){
+	box.style.backgroundColor = "black";
+	//box.textContent = "Ha You Got Me";
+	//box.style.textAlign = "center";
+	//box.style.textColor = "white";
+    }
+    else{
+	box.textContent = "";
+    }
 };
 
 /*
 your OTHER FXNS
 
 */
+
+var maxDistance = function(){
+    dist1 = distance(targetX, targetY, boxWidth, (-1 * boxHeight));
+    dist2 = distance(targetX, targetY, 0,(-1 * boxHeight));
+    dist3 = distance(targetX, targetY, boxWidth, 0);
+    dist4 = distance(targetX, targetY, 0, 0);
+    return Math.max(dist1, dist2, dist3, dist4);
+};
+
+var isCloseEnough = function(x1, y1){
+    if(distance(x1, y1, targetX, targetY) < 35){
+	return true;
+    }
+    else{
+	return false;
+    }
+}
+
+console.log(maxDistance());
 
 box.addEventListener("mousemove", findIt);
 
